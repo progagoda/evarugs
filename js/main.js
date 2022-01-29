b74e69a6871c952a366f30b3982dc257e9dc56f7
JavaMaster <71312371+progagoda@users.noreply.github.com>
2022-01-29 13:38:46 +0300

    last_day

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const mainfunc = () => {
+
+    window.onload = function () {
+        jQuery("#user-city").text(ymaps.geolocation.city);
+        jQuery("#user-city2").text(ymaps.geolocation.city);
+    }
 const swiper = new Swiper('.swiper', {
     slidesPerView: 3,
     loop: true,
@@ -9,6 +15,41 @@ const swiper = new Swiper('.swiper', {
     breakpoint:{
         320:{
             slidesPerView:1
-        }
+        },
+        891: {
+            slidesPerView: 2,
+            spaceBetween: 40
+          },
+          1200: {
+              slidesPerView: 3,
+              spaceBetween: 70
+            }
     }
   });
+  const modal__window = document.querySelector('.modal');
+const body = document.querySelector('body');
+const modal__btn = document.querySelector('.main-display__button');
+
+modal__btn.addEventListener('click', () => {
+  modal__window.classList.add('active');
+  body.classList.add('lock');
+});
+
+modal__window.addEventListener('click', (e) => {
+  const is__modal = e.target.closest('.modal__inner');
+  if (!is__modal) {
+    modal__window.classList.remove('active');
+    body.classList.remove('lock');
+  }
+})
+
+document.addEventListener('keyup', (e) => {
+  if(e.code === "Escape") {
+    modal__window.classList.remove('active');
+    body.classList.remove('lock');
+  }
+})
+
+}
+
+mainfunc();
